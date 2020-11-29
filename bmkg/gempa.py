152aa730e008cdffbbadce52055dc32e3409645c
muhammad hanif <moehammadhanif@gmail.com>
2020-11-29 11:41:32 +0700

    try and except

--- bmkg/gempa.py
+++ bmkg/gempa.py
@@ -3,7 +3,7 @@
 # @email        : moehammadhanif@gmail.com
 # @home page    : https://hanifmu.com
 # @create date  : 2020-11-29 09:26:19
-# @modify date  : 2020-11-29 09:26:19
+# @modify date  : 2020-11-29 11:24:56
 #
 
 import requests
@@ -16,34 +16,49 @@ class Gempa:
         """
         Data gempa bumi magnitudo 5.0+ terkini.
         """
-        data = requests.get("https://data.bmkg.go.id/autogempa.xml")
-        xml = xmltodict.parse(data.content)
 
-        return json.dumps(xml)
+        url = "https://data.bmkg.go.id/autogempa.xml"
+
+        return self.__getData(url)
 
     def tsunami_terkini(self):
         """
         Data tsunami terkini.
         """
-        data = requests.get("https://data.bmkg.go.id/lasttsunami.xml")
-        xml = xmltodict.parse(data.content)
 
-        return json.dumps(xml)
+        url = "https://data.bmkg.go.id/lasttsunami.xml"
+
+        return self.__getData(url)
 
     def m_5(self):
         """
         Data gempa bumi magnitudo 5.0+.
         """
-        data = requests.get("https://data.bmkg.go.id/gempaterkini.xml")
-        xml = xmltodict.parse(data.content)
 
-        return json.dumps(xml)
+        url = "https://data.bmkg.go.id/gempaterkini.xml"
+
+        return self.__getData(url)
 
     def dirasakan(self):
         """
         Data gempa bumi dirasakan+.
         """
-        data = requests.get("https://data.bmkg.go.id/gempaterkini.xml")
-        xml = xmltodict.parse(data.content)
 
-        return json.dumps(xml)
+        url = "https://data.bmkg.go.id/gempadirasakan.xml"
+
+        return self.__getData(url)
+
+    def __getData(self, url):
+        try:
+            response = requests.get(url)
+            data = xmltodict.parse(response.content)
+
+            return json.dumps(data)
+        except requests.exceptions.HTTPError as errh:
+            return repr(errh)
+        except requests.exceptions.ConnectionError as errc:
+            return repr(errc)
+        except requests.exceptions.Timeout as errt:
+            return repr(errt)
+        except requests.exceptions.RequestException as err:
+            return repr(err)
